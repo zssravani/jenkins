0acb885c300950f4a63baed835dfcb996a08e709
Sravanizelar <82637289+Sravanizelar@users.noreply.github.com>
2021-07-13 12:09:14 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -1,5 +1,30 @@
+folder('CI-Pipelines') {
+  displayName('CI Pipelines')
+  description('CI Pipelines')
+}
 
-  pipelineJob('frontend-ci') {
+pipelineJob('frontend-ci') {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+              'url'('https://github.com/zssravani/frontend.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main/*')
+            }
+          }
+        }
+        'scriptPath'('Jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+  pipelineJob('CI-Pipelines/frontend-ci') {
     configure { flowdefinition ->
       flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
